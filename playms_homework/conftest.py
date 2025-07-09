586047dc292c11f195efe6a81c8f735df29f0b9a
薛華慶, james.hsueh <james.hsueh@miniasp.com>
2025-07-10 00:12:26 +0800

    feat: Add initial project setup with configurations and base structure

---
+++ playms_homework/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from playms_homework.users.models import User
+from playms_homework.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ run_db.sh
@@ -0,0 +1,1 @@
+docker run --name my_postgres -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydb -p 5432:5432 -d postgres
